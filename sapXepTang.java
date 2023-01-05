906a67c3060fa17e83548e4c8934acc252530b1e
unknown <toan.hust@gmail.com>
2023-01-06 01:31:53 +0700

    thêm sapXepGiam

--- sapXepTang.java
+++ sapXepTang.java
@@ -1,8 +1,8 @@
-public class sapXep {
+public class sapXepTang {
     public static void main(String[] args) {
         int arr[] = {3, 4, 10, 4, 5, 6, 4, 2, 4, 5, 6};
         int trungGian;
-        for (int i = 0; i < arr.length-1; i++) {
+        for (int i = 0; i < arr.length - 1; i++) {
             for (int j = 0; j < arr.length - i - 1; j++) {
                 if (arr[j] > arr[j + 1]) {
                     trungGian = arr[j];
@@ -11,7 +11,7 @@ public class sapXep {
                 }
             }
         }
-        for (int a=0; a<arr.length;a++) {
+        for (int a = 0; a < arr.length; a++) {
             System.out.println(arr[a]);
         }
     }
